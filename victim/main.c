c661bfbe43aa0aed4f81a3ea76d5e6ebd2bb91ec
dev-zzo <gauri@tut.by>
2014-11-10 18:31:40 +0100

    victim: added few test cases. build fixes.

--- victim/main.c
+++ victim/main.c
@@ -1,4 +1,28 @@
 #include <Windows.h>
+#include <tchar.h>
+
+/* NULL pointer dereference: write. */
+int tc_00001(void)
+{
+    char *p = NULL;
+    *p = 0xDEADC0DE;
+    return 0;
+}
+
+/* NULL pointer dereference: read. */
+int tc_00002(void)
+{
+    char *p = NULL;
+    return *p;
+}
+
+/* Stack smash with 0x41 */
+int tc_00003(void)
+{
+    char buf[4];
+    memset(buf, 0x41, 32);
+    return 0;
+}
 
 int CALLBACK WinMain(
     HINSTANCE hInstance,
@@ -6,5 +30,14 @@ int CALLBACK WinMain(
     LPSTR lpCmdLine,
     int nCmdShow)
 {
-    return 0;
+    int tc_number;
+
+    tc_number = _ttoi(lpCmdLine);
+    switch (tc_number) {
+    case 1: return tc_00001();
+    case 2: return tc_00002();
+    case 3: return tc_00003();
+    default:
+        return 1;
+    }
 }
